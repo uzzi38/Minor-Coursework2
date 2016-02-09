b1d609a5efd700cae825b08a7553c213006e88d8
uzzi38 <uzzi380@gmail.com>
2016-02-09 11:54:16 +0000

    Push

--- Minor-Coursework2/src/philipusman/Camera.java
+++ Minor-Coursework2/src/philipusman/Camera.java
@@ -1,9 +1,14 @@
 package philipusman;
-
+import javax.swing.JFrame;
 import javax.swing.JSlider;
 
 public class Camera {
-	public static void Main (String[] Args){
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
 		JSlider yesNo = new JSlider(0,15);
+		frame.add(yesNo);
+
 	}
+
 }
