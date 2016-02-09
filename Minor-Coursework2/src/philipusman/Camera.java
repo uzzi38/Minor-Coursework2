6bb9d1ed55bb3aa4fb49ccbdbb0b60eb8c940d8b
uzzi38 <uzzi380@gmail.com>
2016-02-09 12:09:42 +0000

    Pushed

--- Minor-Coursework2/src/philipusman/Camera.java
+++ Minor-Coursework2/src/philipusman/Camera.java
@@ -6,7 +6,16 @@ public class Camera {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 300);
+		
 		JSlider yesNo = new JSlider(0,15);
+		yesNo.setMajorTickSpacing(1);
+		yesNo.setPaintTicks(true);
+		yesNo.setSnapToTicks(true);
+		yesNo.setPaintLabels(true);
+		
+		
 		frame.add(yesNo);
 		frame.setVisible(true);
 	}
