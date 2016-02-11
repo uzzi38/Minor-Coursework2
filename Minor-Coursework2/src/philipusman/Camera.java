f8c430d16b216fae0dd1fa52cb6ca57285580ad4
uzzi38 <uzzi380@gmail.com>
2016-02-11 12:20:48 +0000

    Messing around with HexConv. Trying to get it to read the file itself

--- Minor-Coursework2/src/philipusman/Camera.java
+++ Minor-Coursework2/src/philipusman/Camera.java
@@ -1,14 +1,20 @@
 package philipusman;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
 
 import javax.swing.JLabel;
 import javax.swing.JSlider;
 
 public class Camera {
 	private JSlider slider;
+	private HashMap<Integer, Character> map;
 	public Camera () {
 		Hashtable<Integer, JLabel> labels = new Hashtable<>();
+		map = new HashMap<>();
 		for (int i = 0; i < 10; i++){
 			JLabel label = new JLabel(Integer.toString(i));
 			labels.put(i, label);
@@ -27,10 +33,22 @@ public class Camera {
 		slider.setPaintLabels(true);
 		slider.setValue(0);
 		slider.setLabelTable(labels);
-
+		
+		map.put(10, new Character('A'));
+		map.put(11, new Character('B'));
+		map.put(12, new Character('C'));
+		map.put(13, new Character('D'));
+		map.put(14, new Character('E'));
+		map.put(15, new Character('F'));
+		
 	}
 	public JSlider returnSlider(){
 		return slider;
 	}
-	
+	public void setPosition(List<Integer> l){
+		for (int i = 0; i< l.size(); i++){
+			int j = l.get(i);
+			
+		}
+	}
 }
--- Minor-Coursework2/src/philipusman/HexConv.java
+++ Minor-Coursework2/src/philipusman/HexConv.java
@@ -1,5 +1,7 @@
 package philipusman;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,9 +12,9 @@ public class HexConv {
     char[] charLookup = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     String[] hexLookup = {"41", "42", "43", "44", "45", "46", "47", "48", "49", "4A", "4B", "4C", "4D", "4E", "4F", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "5A"};
 
-    public ArrayList<String> hexConvert(String in){
+    public ArrayList<String> hexConvert(String in) throws FileNotFoundException{
         ArrayList<String> out = new ArrayList<String>();
-        Scanner scnr = new Scanner (in);
+        Scanner scnr = new Scanner (new File("ascii_table.csv"));
         String current = scnr.next();
         for (int i = 0; i < current.length(); i++){
             //String split into individual characters
