4368148c318f53c7012867ad48cdd02d03289166
uzzi38 <uzzi380@gmail.com>
2016-02-11 11:31:56 +0000

    Pretty big push here.
    I've set it up as a regular class, I'll create the main in a bit

--- Minor-Coursework2/src/philipusman/Camera.java
+++ Minor-Coursework2/src/philipusman/Camera.java
@@ -1,17 +1,13 @@
 package philipusman;
 
 import java.util.Hashtable;
-import javax.swing.JFrame;
+
 import javax.swing.JLabel;
 import javax.swing.JSlider;
 
 public class Camera {
-
-	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(800, 300);
-		
+	private JSlider slider;
+	public Camera () {
 		Hashtable<Integer, JLabel> labels = new Hashtable<>();
 		for (int i = 0; i < 10; i++){
 			JLabel label = new JLabel(Integer.toString(i));
@@ -24,16 +20,17 @@ public class Camera {
 		labels.put(14, new JLabel("E"));
 		labels.put(15, new JLabel("F"));
 
-		JSlider slider = new JSlider(0,15);
+		slider = new JSlider(0,15);
 		slider.setMajorTickSpacing(1);
 		slider.setPaintTicks(true);
 		slider.setSnapToTicks(true);
 		slider.setPaintLabels(true);
 		slider.setValue(0);
 		slider.setLabelTable(labels);
-		
-		frame.add(slider);
-		frame.setVisible(true);
-	}
 
+	}
+	public JSlider returnSlider(){
+		return slider;
+	}
+	
 }
