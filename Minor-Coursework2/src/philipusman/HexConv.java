a2c8546dbd623f673b0d5c73ec2de65d9603f63b
uzzi38 <uzzi380@gmail.com>
2016-02-11 13:00:30 +0000

    Almost ready :)

--- Minor-Coursework2/src/philipusman/HexConv.java
+++ Minor-Coursework2/src/philipusman/HexConv.java
@@ -12,13 +12,17 @@ public class HexConv {
     char[] charLookup = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     String[] hexLookup = {"41", "42", "43", "44", "45", "46", "47", "48", "49", "4A", "4B", "4C", "4D", "4E", "4F", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "5A"};
 
+
+    public HexConv(){}
     public ArrayList<String> hexConvert(String in) throws FileNotFoundException{
         ArrayList<String> out = new ArrayList<String>();
-        Scanner scnr = new Scanner (in);
-        String current = scnr.next();
-        for (int i = 0; i < current.length(); i++){
+        in.toUpperCase();
+        for (int i = 0; i < in.length(); i++){
             //String split into individual characters
-            input.add(current.charAt(i));
+            input.add(in.charAt(i));
+        }
+        for (int i = 0; i< input.size(); i++){
+        	out.add(csvScanner(input.get(i)));
         }
         //Iterate input and correlate to characters in charLookup
         for (int n = 0; n < input.size(); n++){
@@ -28,12 +32,30 @@ public class HexConv {
                 }
             }
         }
-        scnr.close();
         //Outputs array of Strings, with each element being Hex code for respective character in input
         return out;
     }
-
-    public HexConv(){
-
+    private String csvScanner(Character in) throws FileNotFoundException{
+    	Scanner tableScanner = new Scanner (new File("ascii_table.csv"));
+        tableScanner.useDelimiter(",");
+        tableScanner.close();
+        while(tableScanner.hasNextLine()){
+        	Scanner lineScanner = new Scanner(tableScanner.nextLine());
+        	String dec = lineScanner.next();
+        	String oct = lineScanner.next();
+        	String hex = lineScanner.next();
+        	String bin = lineScanner.next();
+        	String symbol = lineScanner.next();
+        	String htmlNum = lineScanner.next();
+        	String htmlName = lineScanner.next();
+        	lineScanner.close();
+        	if(in.equals(symbol)){
+        		return hex;
+        	}
+        	else{
+        		return "?";
+        	}
+        }
+        return "?";
     }
 }
