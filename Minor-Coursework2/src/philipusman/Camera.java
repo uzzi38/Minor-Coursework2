d190fe16938dc9c7c7a36013085ed557d215e53f
uzzi38 <uzzi380@gmail.com>
2016-02-15 10:46:06 +0000

    Bit of working integrating HexConv and Camera.

--- Minor-Coursework2/src/philipusman/Camera.java
+++ Minor-Coursework2/src/philipusman/Camera.java
@@ -1,17 +1,24 @@
 package philipusman;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
-import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JLabel;
 import javax.swing.JSlider;
 
 public class Camera {
 	private JSlider slider;
-	private HashMap<Integer, Character> map;
+	private HashMap<Character, Integer> map;
+	private ArrayList<String> chars; //Holds the value of the string in hex
+	private ArrayList<Character> input = new ArrayList<Character>(); //The string that will be entered split up
+	/**
+	 * 
+	 */
 	public Camera () {
 		Hashtable<Integer, JLabel> labels = new Hashtable<>();
 		map = new HashMap<>();
@@ -34,21 +41,71 @@ public class Camera {
 		slider.setValue(0);
 		slider.setLabelTable(labels);
 		
-		map.put(10, new Character('A'));
-		map.put(11, new Character('B'));
-		map.put(12, new Character('C'));
-		map.put(13, new Character('D'));
-		map.put(14, new Character('E'));
-		map.put(15, new Character('F'));
-		
+		map.put('A', 10);	map.put('B', 11);	map.put('C', 12);	map.put('D', 13);	
+		map.put('E', 14);	map.put('F', 15);	map.put('0', 0);	map.put('1', 1);	
+		map.put('2', 2);	map.put('3', 3);	map.put('4', 4);	map.put('5', 5);
+		map.put('6', 6);	map.put('7', 7);	map.put('8', 8);	map.put('9', 9);
 	}
+	/**
+	 * 
+	 * @return
+	 */
 	public JSlider returnSlider(){
 		return slider;
 	}
-	public void setPosition(List<Integer> l){
-		for (int i = 0; i< l.size(); i++){
-			int j = l.get(i);
-			
+	public void setPosition(String in) throws FileNotFoundException, InterruptedException{
+		chars = hexConvert(in);
+		for (int i = 0; i < chars.size(); i++){
+			String s = chars.get(i);
+			for (int j = 0; j < 2; j++){
+				Character c = new Character(s.charAt(j));
+				slider.setValue(map.get(c));
+				Thread.sleep(500);
+			}
 		}
 	}
+	/**
+	 * 
+	 * @param in
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	private ArrayList<String> hexConvert(String in) throws FileNotFoundException{
+        ArrayList<String> out = new ArrayList<String>();
+        in.toUpperCase();
+        for (int i = 0; i < in.length(); i++){
+            //String split into individual characters
+            input.add(in.charAt(i));
+        }
+        for (int i = 0; i< input.size(); i++){
+        	out.add(csvScanner(input.get(i)));
+        }
+        //Iterate input and correlate to characters in charLookup
+        return out;
+    }
+	/**
+	 * 
+	 * @param in
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	private String csvScanner(Character in) throws FileNotFoundException{
+    	Scanner tableScanner = new Scanner (new File("ascii_table.csv"));
+        tableScanner.useDelimiter(",");
+        while(tableScanner.hasNextLine()){
+        	Scanner lineScanner = new Scanner(tableScanner.nextLine());
+        	lineScanner.next();
+        	lineScanner.next();
+        	String hex = lineScanner.next();
+        	lineScanner.next();
+        	String symbol = lineScanner.next();
+        	lineScanner.close();
+        	tableScanner.close();
+        	if(in.equals(symbol)){
+        		return hex;
+        	}
+        }
+        tableScanner.close();
+        return "?";
+    }
 }
--- Minor-Coursework2/src/philipusman/HexConv.java
+++ Minor-Coursework2/src/philipusman/HexConv.java
@@ -8,11 +8,8 @@ import java.util.Scanner;
 public class HexConv {
 
     //Pass input with scanner as tokens into input array
-    ArrayList<Character> input = new ArrayList<Character>();
-    char[] charLookup = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    String[] hexLookup = {"41", "42", "43", "44", "45", "46", "47", "48", "49", "4A", "4B", "4C", "4D", "4E", "4F", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "5A"};
-
-
+    private ArrayList<Character> input = new ArrayList<Character>();
+   
     public HexConv(){}
     public ArrayList<String> hexConvert(String in) throws FileNotFoundException{
         ArrayList<String> out = new ArrayList<String>();
@@ -25,34 +22,25 @@ public class HexConv {
         	out.add(csvScanner(input.get(i)));
         }
         //Iterate input and correlate to characters in charLookup
-        for (int n = 0; n < input.size(); n++){
-            for (int x = 0; x < charLookup.length; x++){
-                if (input.get(n) == charLookup[x]){
-                    out.add(hexLookup[x]);
-                }
-            }
-        }
-        //Outputs array of Strings, with each element being Hex code for respective character in input
         return out;
     }
     private String csvScanner(Character in) throws FileNotFoundException{
     	Scanner tableScanner = new Scanner (new File("ascii_table.csv"));
         tableScanner.useDelimiter(",");
-        tableScanner.close();
         while(tableScanner.hasNextLine()){
         	Scanner lineScanner = new Scanner(tableScanner.nextLine());
-        	String dec = lineScanner.next();
-        	String oct = lineScanner.next();
+        	lineScanner.next();
+        	lineScanner.next();
         	String hex = lineScanner.next();
-        	String bin = lineScanner.next();
+        	lineScanner.next();
         	String symbol = lineScanner.next();
-        	String htmlNum = lineScanner.next();
-        	String htmlName = lineScanner.next();
         	lineScanner.close();
+        	tableScanner.close();
         	if(in.equals(symbol)){
         		return hex;
         	}
         }
+        tableScanner.close();
         return "?";
     }
 }
