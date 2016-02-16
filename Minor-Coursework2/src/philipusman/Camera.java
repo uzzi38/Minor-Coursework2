c570065b70dfc2e3a4aee856b879b97eb246bfac
Philip <philip.to@kcl.ac.uk>
2016-02-16 11:34:26 +0000

    General comments and Javadoc

--- Minor-Coursework2/src/philipusman/Camera.java
+++ Minor-Coursework2/src/philipusman/Camera.java
@@ -1,6 +1,5 @@
 package philipusman;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -12,21 +11,28 @@ import javax.swing.JLabel;
 import javax.swing.JSlider;
 
 public class Camera {
+    //New JSlider created
 	private JSlider slider;
+    //New HashMap created
 	private HashMap<Character, Integer> map = new HashMap<>();
+    //Import of ASCII csv file
 	private final File asciiTable = new File("ascii_table.csv");
+    //New HashMap created
 	private HashMap<String, String> symbolToHex = new HashMap<>();
-	private ArrayList<String> chars; //Holds the value of the string in hex
-	private ArrayList<Character> input = new ArrayList<Character>(); //The string that will be entered split up
-	/**
-	 * 
-	 */
+    //Holds value of string in hex
+	private ArrayList<String> chars;
+    //The String that will be entered split up
+	private ArrayList<Character> input = new ArrayList<Character>();
+
 	public Camera () {
+
+        //New labels for slider from 1 to 10
 		Hashtable<Integer, JLabel> labels = new Hashtable<>();
 		for (int i = 0; i < 10; i++){
 			JLabel label = new JLabel(Integer.toString(i));
 			labels.put(i, label);
 		}
+        //New labels for slider from A to F
 		labels.put(10, new JLabel("A"));
 		labels.put(11, new JLabel("B"));
 		labels.put(12, new JLabel("C"));
@@ -34,6 +40,7 @@ public class Camera {
 		labels.put(14, new JLabel("E"));
 		labels.put(15, new JLabel("F"));
 
+        //Denotes there are 15 points on the JSlider
 		slider = new JSlider(0,15);
 		slider.setMajorTickSpacing(1);
 		slider.setPaintTicks(true);
@@ -41,29 +48,30 @@ public class Camera {
 		slider.setPaintLabels(true);
 		slider.setValue(0);
 		slider.setLabelTable(labels);
-		
+
+        //Denotes where the labels are put in accordance to positions of the points
 		map.put('A', 10);	map.put('B', 11);	map.put('C', 12);	map.put('D', 13);	
 		map.put('E', 14);	map.put('F', 15);	map.put('0', 0);	map.put('1', 1);	
 		map.put('2', 2);	map.put('3', 3);	map.put('4', 4);	map.put('5', 5);
 		map.put('6', 6);	map.put('7', 7);	map.put('8', 8);	map.put('9', 9);
 	}
 	/**
-	 * 
-	 * @return
+	 * Returns the slider
+	 * @return slider
 	 */
 	public JSlider returnSlider(){
 		return slider;
 	}
 	
 	/**
-	 * 
+	 * Hex Converter
 	 * @param in
-	 * @return
-	 * @throws FileNotFoundException
+	 * @return Returns the Array
+	 * @throws FileNotFoundException If the file is not present, it throws this exception.
 	 */
 	private ArrayList<String> hexConvert(String str) throws FileNotFoundException{
         csvScanner();
-		ArrayList<String> out = new ArrayList<String>();
+		ArrayList<String> out = new ArrayList<>();
         for (int i = 0; i < str.length(); i++){
             //String split into individual characters
         	input.add(str.charAt(i));
@@ -77,10 +85,9 @@ public class Camera {
         return out;
     }
 	/**
-	 * 
+	 * Scans the ASCII Table csv file
 	 * @param in
-	 * @return
-	 * @throws FileNotFoundException
+	 * @throws FileNotFoundException If the file is not present, it throws this exception.
 	 */
 	private void csvScanner() throws FileNotFoundException{
         Scanner tableScanner = new Scanner(asciiTable);
@@ -103,6 +110,13 @@ public class Camera {
         }
         tableScanner.close();
     }
+
+	/**
+	 *
+	 * @param in
+	 * @throws FileNotFoundException If the file is not present, it throws this exception
+	 * @throws InterruptedException Stops the program for 500 ms, 'Thread.sleep(500)'
+     */
 	
 	public void sendMessage(String in) throws FileNotFoundException, InterruptedException{
 		chars = hexConvert(in);
--- Minor-Coursework2/src/philipusman/Communication.java
+++ Minor-Coursework2/src/philipusman/Communication.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
-import java.io.InterruptedIOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,29 +16,38 @@ public class Communication implements ActionListener{
     private static Camera c;
 
 	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-		JFrame frame = new JFrame();
+
+        //New interface created of size 800x800, with program to stop when window is closed
+        JFrame frame = new JFrame();
 		frame.setLayout(new BorderLayout());
 		frame.setSize(800, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
+		//New slider created and placed on north of border
 		c = new Camera();
 		JSlider slider = c.returnSlider();
 		frame.add(slider, BorderLayout.NORTH);
-		
+
+        //New JButton created on south of border
 		send = new JButton("Send");
 		frame.add(send, BorderLayout.SOUTH);
-		
+
+        //New TextArea created in center of border
 		JTextArea message = new JTextArea();
 		frame.add(message, BorderLayout.CENTER);
 		c.sendMessage("hello");
 		frame.setVisible(true);
 	}
 
+    //Action Listener class implementation so that send button can perform event
     public Communication(){
         send.addActionListener(this);
     }
 
+	/**
+	 *
+	 * @param e
+     */
     public void actionPerformed (ActionEvent e){
         try {
 			c.sendMessage(send.getText());
