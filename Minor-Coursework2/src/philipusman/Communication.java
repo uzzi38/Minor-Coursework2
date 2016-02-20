4739d50c4739a3d09d950ebd8894f3ecea60766c
uzzi38 <uzzi380@gmail.com>
2016-02-20 18:43:12 +0000

    Testing

--- Minor-Coursework2/src/philipusman/Communication.java
+++ Minor-Coursework2/src/philipusman/Communication.java
@@ -3,7 +3,6 @@ package philipusman;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,6 +16,8 @@ public class Communication extends JFrame{
 
     private JButton send;
     private static Camera c;
+    private JSlider slider;
+    private ArrayList<Character> values;
 	public static void main(String[] args) throws InterruptedException, IOException {
 		c = new Camera();
 		Communication co = new Communication(c);
@@ -30,7 +31,7 @@ public class Communication extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Communication co = this;
 		//New slider created and placed on north of border
-		JSlider slider = c.returnSlider();
+		slider = c.returnSlider();
 		add(slider, BorderLayout.NORTH);
 
 		//New TextArea created in center of border
@@ -47,27 +48,44 @@ public class Communication extends JFrame{
 			public void actionPerformed(ActionEvent e) {
 				HashMap<Character,Integer> map= c.returnMap();
 				ArrayList<String> out = new ArrayList<>();
-				co.setTitle("Sending");
 				try {
 					out = c.hexConvert(message.getText());
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
-				for (int i = 0; i < out.size(); i++){
-					String s = out.get(i);
-					System.out.println(s);
-					for (int j = 0; j < s.length(); j++){
-						Character c = new Character(s.charAt(j));
-						slider.setValue(map.get(c).intValue());
+				try {
+					values = co.valueCreate(out);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+				co.setTitle("Sending...");
+				new Thread(){
+					public void run (){
 						try {
-							Thread.sleep(1000);
-						} catch (InterruptedException e1) {
-							e1.printStackTrace();
+							for(int k = 0; k < values.size(); k++){	
+								co.pointTowards(values.get(k));
+							}
+						} catch (InterruptedException e) {
+							e.printStackTrace();
 						}
 					}
-				}
+				}.start();
 				co.setTitle("Communication");
-			}
+    		}	
 		});
+    }
+    private ArrayList<Character> valueCreate(ArrayList<String> out) throws InterruptedException{
+		ArrayList<Character> values = new ArrayList<>();
+    	for (int i = 0; i < out.size(); i++){
+    		String s = out.get(i);
+    		for (int j = 0; j< s.length(); j++){
+    			values.add(s.charAt(j));
+    		}
+    	}
+    	return values;
+    }
+    private void pointTowards(int i) throws InterruptedException{
+    	slider.setValue(i);
+    	Thread.sleep(1000);
     }
 }
